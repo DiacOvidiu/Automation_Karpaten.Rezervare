4f2dc948437095c6e16b1b806f9a09dcae5e228e
Diac Ovidiu <112970634+DiacOvidiu@users.noreply.github.com>
2023-06-26 19:06:22 +0300

    Update Rezervare.py

--- Rezervare.py
+++ Rezervare.py
@@ -1,18 +1,47 @@
 from selenium import webdriver
+# Această linie de cod importă modulul webdriver din pachetul selenium.
+# Selenium este o bibliotecă populară în Python utilizată pentru a automatiza acțiunile într-un browser web.
+# webdriver este clasa principală din pachetul selenium și reprezintă o interfață către diferite browsere web, cum ar fi Chrome, Firefox, Safari etc.
+
 from selenium.webdriver.chrome.service import Service
+#importă clasa Service din modulul selenium.webdriver.chrome.service.
+# Service este responsabilă de gestionarea serviciului ChromeDriver, care este necesar pentru a comunica cu browserul Chrome folosind Selenium.
+
 from selenium.webdriver.common.by import By
+# By este utilizată pentru a specifica diferite metode de localizare a elementelor pe o pagină web în Selenium.
+# Aceste metode includ, printre altele, identificarea elementelor prin selectori CSS, XPath, nume de clasă, identificator unic etc.
+# Importând clasa By, vei putea utiliza aceste metode de localizare pentru a interacționa cu elementele de pe o pagină web
+
 from selenium.webdriver.support.ui import WebDriverWait
+# WebDriverWait este utilizată pentru a aștepta anumite condiții în Selenium.
+# Acesta permite programului să aștepte până când anumite elemente devin vizibile, clicabile, sau îndeplinesc alte criterii specifice înainte de a continua execuția.
+
 from selenium.webdriver.support import expected_conditions as EC
+# expected_conditions conține o serie de condiții predefinite pe care le poți utiliza împreună cu WebDriverWait pentru a aștepta anumite stări sau evenimente
+# pe o pagină web. Aceste condiții pot fi, de exemplu, verificarea dacă un element este vizibil, clicabil, prezent în DOM sau să îndeplinească anumite proprietăți.
+
 from selenium.webdriver.support.select import Select
+# Prin importarea clasei Select, vei putea utiliza aceste funcționalități pentru a automatiza interacțiunea cu dropdown-urile în cadrul testelor
+
 from time import sleep
+# sleep este utilizată pentru a introduce o pauză în execuția programului pentru un anumit număr de secunde.
+# Această funcție este utilă în automatizarea testelor web când este necesar să aștepți un anumit interval de timp între diferite acțiuni sau să aștepți ca o
+# pagină să se încarce complet înainte de a continua cu acțiunile următoare.
 
 # Specificați calea către driverul Chrome (chromedriver)
 chrome_driver_path = "D:\chromedriver.exe"
 
 # Configurați opțiunile pentru Selenium și specificați calea către driverul Chrome
 options = webdriver.ChromeOptions()
+# ChromeOptions este utilizată pentru a configura și personaliza opțiunile browserului Chrome atunci când este utilizat cu Selenium.
+# Prin crearea unui obiect options, vei putea seta diferite opțiuni pentru browserul Chrome, cum ar fi setările de navigare, opțiunile de afișare, etc
+
 # options.add_argument('--headless')  # Rulați în modul headless pentru a nu afișa browserul
+
 driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+# service=Service(chrome_driver_path) indică că serviciul ChromeDriver trebuie să fie inițializat utilizând calea către fișierul executabil ChromeDriver.
+# Acesta este necesar pentru a putea comunica cu browserul Chrome.
+# options=options specifică că opțiunile obiectului options definite anterior trebuie utilizate pentru configurarea browserului Chrome asociat obiectului driver.
 
 # Maximizam fereastra
 driver.maximize_window()
@@ -41,10 +70,15 @@ sleep(3)
 
 try:
     # Alegeți o cameră
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.label-title"))).click()
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "div.label-title"))).click()
+    #  utilizează WebDriverWait pentru a aștepta ca un element să devină clicabil pe pagină și apoi face clic pe el.
+    # In cazul nostru se apasa pe o camera de Hotel
     sleep(3)
+
     WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "input[type='submit'][name='submit'][value='REZERVA ACUM'][class='btn btn-primary btn-md-block']"))).click()
+    # Se apasa pe butonul REZERVA ACUMM
     sleep(3)
     # Completați detaliile rezervării (exemplu: completați numele și adresa de e-mail)
     WebDriverWait(driver, 10).until(
@@ -61,13 +95,20 @@ try:
 
     # Executați un script JavaScript pentru a face clic pe element
     driver.execute_script("arguments[0].click();", element)
+
     #Bifam optiunea ca suntem persoana fizica
     sleep(3)
+    # Completam adresa
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "fnet_karpaten_reservation_client_address"))).send_keys("Str.Vamii, nr.1")
-    #Selectam judetul Sibiu din lista
+
+    # utilizează metoda By.ID pentru a localiza elementul după atributul ID, având valoarea "fnet_karpaten_reservation_client_county".
     select_element = Select(driver.find_element(By.ID, "fnet_karpaten_reservation_client_county"))
+
+    #Selectam judetul Sibiu din lista
     select_element.select_by_value("Sibiu")
     sleep(3)
+
+    # așteaptă ca elementul identificat de atributul ID "fnet_karpaten_reservation_client_city" să devină clicabil și apoi introduce textul "Dumbraveni" în acel element
     WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.ID, "fnet_karpaten_reservation_client_city"))).send_keys("Dumbraveni")
 
@@ -75,12 +116,7 @@ try:
     label_element = driver.find_element(By.CSS_SELECTOR, "label[for='apelativ-1-dl']")
     label_element.click()
 
-    # In cazul in care nu se autocompleteaza cu datele de mai sus, activam cele doua comenzi de mai jos
-    # WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.ID, "nume-1"))).send_keys("Diac")
-    # WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.ID, "prenume-1"))).send_keys("Ovidiu")
-    # sleep(3)
-
-    # Completam data nasterii
+    # Cautam campul Data nasterii si completam
     data_nasterii_element = driver.find_element(By.ID, "data-nasterii-1")
     data_nasterii_element.send_keys("15/03/1998")
     sleep(3)
@@ -89,13 +125,15 @@ try:
     label_element = driver.find_element(By.CSS_SELECTOR, "label[for='documentType-1-pas-temp']")
     label_element.click()
 
-    # Completam campul numar document
+    # Cautam si completam campul numar document
     element = driver.find_element(By.ID, "documentNumber-1")
     element.send_keys("987654321")
     sleep(3)
 
-    # Completam data expirarii a pasaportului
-    data_nasterii = "25/04/2027"
+    # Cautam si completam data expirarii a pasaportului
+    data_nasterii = "25/04/2027" # am atribuit o valoare in variabila data_nasterii
+    # așteaptă ca elementul cu atributul ID "documentExpirationDate-1" să fie prezent în DOM și apoi introduce valoarea data_nasterii
+    # poți identifica elemente pe pagină, obține sau seta valori ale atributelor, adăuga sau șterge elemente și efectua alte operații de interacțiune cu conținutul paginii.
     element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "documentExpirationDate-1")))
     element.send_keys(data_nasterii)
     sleep(3)
@@ -144,6 +182,7 @@ try:
 
     # Scriem un mesaj pentru rezervarea noastra
     mesaj = "Aceasta este o rezervare de TEST. Rog programatorii sa stearga din baza de date aceasta rezervare. Multumesc"
+    # In variabila mesaj punem mesajul dorit
     element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "fnet_karpaten_reservation_message")))
     element.send_keys(mesaj)
     sleep(3)
@@ -151,10 +190,13 @@ try:
     # Sunt de acord cu termenii si conditiile, contractul cu turistul si cu prelucrarea datelor cu caracter personal.
     try:
         # Caută elementul după ID
+        # încearcă să găsească un element pe pagină cu atributul ID "termeni-si-conditii", iar în cazul în care nu se găsește, este tratată excepția NoSuchElementException.
         element = driver.find_element(By.ID, "termeni-si-conditii")
     except NoSuchElementException:
         try:
             # Caută elementul după nume
+            #  încearcă să găsească un element pe pagină cu atributul NAME "conditions_acceptance",
+            #  verifica dacă este un checkbox și tratează excepția NoSuchElementException în cazul în care elementul nu este găsit sau nu este un checkbox.
             element = driver.find_element(By.NAME, "conditions_acceptance")
             if element.get_attribute("type") != "checkbox":
                 raise NoSuchElementException("Elementul găsit nu este un checkbox.")
@@ -169,7 +211,6 @@ try:
     # Faceți clic pe butonul "Rezervați acum"
     WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.ID, "reservationFormSubmitButton"))).click()
     sleep(3)
-    # Așteptați confirmarea rezervării
 
     #Așteptați confirmarea rezervării
     try:
@@ -183,10 +224,11 @@ try:
     except Exception as e:
         import traceback
 
+    #  se utilizează traceback.print_exc() pentru a afișa urma de execuție a excepției generate,
+        #  inclusiv tipul excepției și informațiile despre locul în care a fost generată.
         print("A apărut o eroare în timpul rezervării:")
         traceback.print_exc()
 
 finally:
     # Închideți browser-ul și încheiați sesiunea
     driver.quit()
-
